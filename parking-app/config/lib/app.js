7414d8a3f0bdbe8a081a6da3bbbc13d0b75e1b88
Sam Eshwar <sam@codegarage.us>
2017-06-02 09:07:53 -0700

    Added middlewares for logs

--- parking-app/config/lib/app.js
+++ parking-app/config/lib/app.js
@@ -1,14 +1,12 @@
 'use strict';
 
 var express = require('express'),
-    fs = require('fs'),
-    path = require('path'),
-    morgan = require('morgan');
+    logger = require('./logger');
 
 
 module.exports.start = function(msg){
     var app = this.init();
-        this.initLogger(app);
+        logger.initLogger(app);
 
     app.get('/', function (req, res) {
         res.send('hello, world!')
@@ -16,19 +14,11 @@ module.exports.start = function(msg){
 
     var port = 9000;
     app.listen(port, function(){
-        console.log("Listening on port :: "+port);
+       $logger.info("Listening on port :: "+port);
     });
 };
 
 module.exports.init = function (){
     var app = express ();
     return app;
-};
-
-module.exports.initLogger = function(app){
-    // create a write stream (in append mode)
-    var accessLogStream = fs.createWriteStream(path.join(__dirname || process.cwd(), '../../logs/access.log'), {flags: 'a'})
-
-// setup the logger
-    app.use(morgan('combined', {stream: accessLogStream}))
 };
---
+++ parking-app/config/lib/logger.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    morgan = require('morgan'),
+    winston = require('winston'),
+    logPath = '../../logs';
+
+module.exports.initLogger = function(app){
+    initMorgan(app);
+    initWinston(app);
+};
+
+function initMorgan(app){
+    // create a write stream (in append mode)
+    var accessLogStream = fs.createWriteStream(path.join(__dirname || process.cwd(), logPath+'/access.log'), {flags: 'a'})
+    // setup the logger
+    app.use(morgan('combined', {stream: accessLogStream}))
+
+}
+
+function initWinston(app){
+
+    global.$logger = new (winston.Logger)({
+        transports: [
+            new winston.transports.File({
+                filename: 'logs/application.log',
+                json: false,
+                timestamp: function() {
+                    return new Date().toLocaleString("en-US");
+                },
+                formatter: function(options) {
+                    // Return string will be passed to logger.
+                    return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (options.message ? options.message : '') +
+                        (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+                }
+            }),
+            new winston.transports.Console({
+                handleExceptions: true,
+                json: false,
+                timestamp: function() {
+                    return new Date().toLocaleString("en-US");
+                },
+                formatter: function(options) {
+                    // Return string will be passed to logger.
+                    return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (options.message ? options.message : '') +
+                        (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+                }
+            })
+        ],
+        exceptionHandlers: [
+            new winston.transports.File({ filename: 'logs/exceptions.log' })
+        ]
+    });
+}
